9fdcd84e5942193260d5e00c5fffe1d02712cb07
Yuta kun <63550914+GitHub890@users.noreply.github.com>
2022-07-04 14:14:29 +0600

    Update userinfo_sql.py

--- SaitamaRobot/modules/sql/userinfo_sql.py
+++ SaitamaRobot/modules/sql/userinfo_sql.py
@@ -1,7 +1,7 @@
 import threading
 
 from SaitamaRobot.modules.sql import BASE, SESSION
-from sqlalchemy import Column, Integer, UnicodeText
+from sqlalchemy import Column, BigInteger, UnicodeText
 
 
 class UserInfo(BASE):
